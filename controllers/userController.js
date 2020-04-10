17d9ee3ef8115c76b6d5d17ad51bd09d4ab320a8
devmrwor <michaelryanrm18@gmail.com>
2020-04-10 12:09:17 +0200

    add basic validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
+const User = require("../models/User");
+
 exports.login = function () {};
 
 exports.logout = function () {};
 
 exports.register = function (req, res) {
-  res.send("Thanks for trying to register.");
+  let user = new User(req.body);
+  user.register();
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("Congrats, there are no errors.");
+  }
 };
 
 exports.home = function (req, res) {
